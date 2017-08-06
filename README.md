0b9bd1f8eb5b93e6b568f9f242641310d942008f
CoderMe <srcii@coderme.com>
2017-08-06 15:27:13 +0300

    Gobright inital release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gobright
+
+## Whats Gobright?
+* gobright a command line program to control screen brightness on linux ( tested on debian 9 ), it can be an alternative to xbacklight.
+
+## Features
+* Minimum brightness, so no matter how you many  times you invoke the command brightness may  not go down certain limit.
+* Maximum brightness, so brightness may not go  above certian limit.
+* Support incrementing/decrementing brightness or set it at certain level
+
+## Home
+* For installation and examples go home 
+* https://red.coderme.com/gobright-control-screen-brightness-from-commandline
+
---
+++ src/gobright.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	brightnessFile = "/sys/class/backlight/intel_backlight/brightness"
+)
+
+var (
+	value      = flag.Int("value", 50, "Set brightness value, or decr/incr brightness by value based on (-incr/-decr) flags")
+	incr       = flag.Bool("incr", false, "Increment brightness percentage by value")
+	decr       = flag.Bool("decr", false, "Decrement brightness percentage by value")
+	brightness = flag.String("file", brightnessFile, "file where to write new brightness value")
+	max        = flag.Int("max", 100, "Max brightness percentage")
+	min        = flag.Int("min", 10, "Min brightness percentage")
+	quiet      = flag.Bool("quiet", false, "Be Quiet")
+)
+
+func init() {
+
+	flag.Parse()
+
+	if *value == 0 || *incr && *decr {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *max > 100 || *max < 0 || *max <= *min {
+		*max = 100
+	}
+
+	if *min < 0 || *min >= *max {
+		*min = 0
+	}
+
+}
+
+func main() {
+	bright, err := getBrightness(*brightness)
+	if err != nil {
+		if !*quiet {
+			fmt.Println("[Error]", err)
+		}
+		os.Exit(1)
+	}
+
+	newBright, err2 := setBrightness(bright)
+
+	if !*quiet {
+
+		if err2 != nil {
+			fmt.Println("[Error]", err2)
+		} else {
+
+			currentVal, _ := getBrightness(*brightness)
+
+			if currentVal != newBright {
+				fmt.Println("[INFO] Brightness kept as", currentVal)
+			} else {
+				fmt.Println("[INFO] Brightness set to", currentVal)
+			}
+		}
+
+	}
+
+}
+
+func getBrightness(file string) (int, error) {
+	oldval, err := ioutil.ReadFile(*brightness)
+	if err != nil {
+		return 0, err
+	}
+
+	bright, err2 := strconv.Atoi(
+		strings.TrimSpace(
+			string(oldval),
+		),
+	)
+	if err2 != nil {
+		return 0, err2
+	}
+
+	return bright, nil
+
+}
+
+func setBrightness(oldVal int) (int, error) {
+	newVal := oldVal
+
+	if *incr {
+		newVal += *value
+	}
+
+	if *decr {
+		newVal -= *value
+	}
+
+	if !*incr && !*decr {
+		newVal = *value
+	}
+
+	if newVal > *max {
+		if !*quiet {
+			fmt.Println("[WARNING] Brightness set to 'max'", *max)
+		}
+		newVal = *max
+	}
+
+	if newVal < *min {
+		if !*quiet {
+			fmt.Println("[WARNING] Brightness set to 'min'", *min)
+		}
+		newVal = *min
+	}
+	strVal := strconv.Itoa(newVal)
+	return newVal, ioutil.WriteFile(*brightness, []byte(strVal), 0744)
+
+}
